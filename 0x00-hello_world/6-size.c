414e0fa67d7c8d1ae7becc26296227c7d25d0b9e
SmartJoojo <martinkojoowusu@gmail.com>
2023-07-17 05:56:48 -0700

    6. Size is not grandeur, and territory does not make a nation mandatory
    cd root

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(a));
-	printf("size of an int: %lu byte(s)\n", sizeof(b));
-	printf("size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("size of a float: %lu byte(s)\n", sizeof(e));
+	printf("size of a char: %lu 1byte(s)\n", sizeof(a));
+	printf("size of an int: %lu 4byte(s)\n", sizeof(b));
+	printf("size of a long int: %lu 8byte(s)\n", sizeof(c));
+	printf("size of a long long int: %lu 8byte(s)\n", sizeof(d));
+	printf("size of a float: %lu 4byte(s)\n", sizeof(e));
 	return (0);
 }
