88341a210170924d38fe635b0d448d755bd4c1e5
SmartJoojo <martinkojoowusu@gmail.com>
2023-07-18 07:04:40 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: always 0 (Success)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+	for (j = 97; j <= 122; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	}
+}
